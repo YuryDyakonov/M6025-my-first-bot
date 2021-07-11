5e3f4cdd9d4e453e9bb69f2b20dc991ba13b3b68
YuryDyakonov <87128989+YuryDyakonov@users.noreply.github.com>
2021-07-11 11:00:49 +0300

    Add logging User Name

--- MyTelegramBot1.py
+++ MyTelegramBot1.py
@@ -3,13 +3,14 @@ import settings
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level = logging.INFO,
-                    filename = 'bot.log'
+                    level=logging.INFO,
+                    filename='bot.log'
 )
 
 
 def start_bot(update: Updater, context: CallbackContext):
     mytext = 'Hello {} I have only /start command'.format(update.message.chat.first_name)
+    logging.info('User {} press /start'.format(update.message.chat.username))
     update.message.reply_text(mytext)
 
 
